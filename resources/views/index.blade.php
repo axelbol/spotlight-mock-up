7bd84f86c7cc3f54efb880916a70a6b683c3eca5
axelbol <axel_lp_alvarado@hotmail.com>
2023-01-26 23:25:43 -0400

    index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,84 @@
 @extends('layouts.main')
 
 @section('content')
-    index here
+    <div class="container mx-auto px-4">
+        <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Popular Games</h2>
+        <div class="popular-games text-sm grid grid-cols-5 gap-12 border-b border-gray-700 pb-16">
+            @for ($i = 0; $i < 10; $i++)
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img src="{{ asset('img/ff7.jpg') }}" alt="game cover" class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-700 rounded-full" style="right: -20px; bottom: -20px">
+                        <div class="font-semibold text-xs flex justify-center items-center h-full">
+                            80%
+                        </div>
+                    </div>
+                </div>
+                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">Final Fantasy VII Remake</a>
+                <div class="text-gray-400 mt-1">Playstation 4</div>
+            </div> <!-- end game -->
+            @endfor
+        </div> <!-- end popular games -->
+
+        <div class="flex my-10">
+            <div class="recently-reviewed w-3/4 mr-32">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+                <div class="recently-reviewed-container space-y-12 mt-8">
+                    @for ($i = 0; $i < 3; $i++)
+                    <div class="game bg-gray-600 rounded-lg shadow-md flex px-6 py-6">
+                        <div class="relative flex-none">
+                            <a href="#">
+                                <img src="{{ asset('img/ghost.jpg') }}" alt="cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px">
+                                <div class="font-semibold text-xs flex justify-center items-center h-full">
+                                    80%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ml-12">
+                            <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-300 mt-4">Ghost Fantasy VII Remake</a>
+                            <div class="text-gray-300 mt-1">Playstation 3</div>
+                            <p class="mt-6 text-gray-300">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ex modi dolor laboriosam quam illo vel quod autem saepe deserunt. Quidem, cumque earum! Libero explicabo qui nulla laboriosam at. Voluptate omnis nobis repellat harum cumque quibusdam illum ducimus, reiciendis recusandae sunt aperiam earum eaque? Reiciendis aliquid quos ex dolorum suscipit!
+                            </p>
+                        </div>
+                    </div> <!-- end game -->
+                    @endfor
+                </div> <!-- end recently reviewed container -->
+            </div> <!-- end recently reviewed -->
+            <div class="most-anticipated w-1/4">
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
+                @for ($i = 0; $i < 4; $i++)
+                <div class="most-anticipated-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ asset('img/cyberpunk.jpg') }}" alt="cover game" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div> <!-- end most anticipated container-->
+                @endfor
+                <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-12">Coming Soon</h2>
+                @for ($i = 0; $i < 4; $i++)
+                <div class="coming-soon-container space-y-10 mt-8">
+                    <div class="game flex">
+                        <a href="#">
+                            <img src="{{ asset('img/luigi.jpg') }}" alt="cover game" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="ml-4">
+                            <a href="#" class="hover:text-gray-300">Cyberpunk 2077</a>
+                            <div class="text-gray-400 text-sm mt-1">Sept 16, 2020</div>
+                        </div>
+                    </div>
+                </div> <!-- end coming soon container-->
+                @endfor
+            </div> <!-- end most anticipated -->
+        </div>
+    </div> <!-- end container -->
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -26,7 +26,28 @@
                         <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
                     </ul>
                 </div>
+                <div class="flex items-center">
+                    <div class="relative">
+                        <input type="text" name="" id="" class="bg-gray-800 text-sm rounded-full w-64 px-3 pl-8 py-1" placeholder="Search...">
+                        <div class="absolute top-0 flex items-center h-full ml-2">
+                            <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg>
+                        </div>
+                    </div>
+                    <div class="ml-6">
+                        <a href="#"><img src="{{ asset('img/avatar.jpg') }}" alt="avatar" class="rounded-full w-8"></a>
+                    </div>
+                </div>
             </nav>
         </header>
+
+        <main class="py-8">
+            @yield('content')
+        </main>
+
+        <footer class="border-t border-gray-600">
+            <div class="container mx-auto px-4 py-6">
+                Powered By <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+            </div>
+        </footer>
     </body>
 </html>
